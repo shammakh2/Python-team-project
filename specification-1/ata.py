23b9a1d74f8a03df8f77a5bd0bb713cfdae3735f
Adam <b9024845@newcastle.ac.uk>
2019-10-31 16:04:30 +0000

    added comments to ata.py

--- specification-1/ata.py
+++ specification-1/ata.py
@@ -9,23 +9,23 @@ class Analyser:
 
     def __init__(self, path=os.path.split(os.path.split(os.path.realpath(__file__))[0])[0] + "\\resources\\txt\\book"
                                                                                              ".txt"):
+        """checks the file passed actually exists"""
         if os.path.isfile(path) and path.endswith(".txt"):
             self.__path = path
         else:
             raise Exception(path + " is not a valid .txt file, ensure the location and file type are correct")
 
-        print(self.__count_words())
-
     def __count_words(self):
+        """counts how many times a word appears in the file"""
         word_count = {}
         with open(self.__path, encoding="utf8") as file:
             for line in file:
                 self.__count_line(word_count, line)
-
         file.close()
         return word_count
 
     def __count_line(self, word_count, line):
+        """counts how many times a word appears in a string"""
         for word in self.__format_string(line).split(" "):
             if word != "":
                 if word in word_count:
@@ -34,10 +34,11 @@ class Analyser:
                     word_count[word] = 1
 
     def __format_string(self, string):
+        """removes non-alphanumeric characters and makes a string lowercase"""
         string = string.lower()
         string = re.sub(r"[^\w]", " ", string)
         string = string.replace("_", "")
         return string
 
-myanalyser = Analyser()
+
 
