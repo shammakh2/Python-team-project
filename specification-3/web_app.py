10e850ea875b87886f03fa77bea07e4ad7367823
b9017955 <b9017955@newcastle.ac.uk>
2019-11-04 10:38:21 +0000

    Web Interface now has two web pages with links between them.

--- specification-3/web_app.py
+++ specification-3/web_app.py
@@ -5,8 +5,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html', title="Homepage", message="hello team 48")
+def page_1():
+    return render_template('index.html', title="Homepage", message="Hello team 48, this is page 1!")
+
+@app.route('/page_2')
+def page_2():
+    return render_template('index.html', title="Page 2", message= "Hello team 48, this is page 2!")
+
 
 
 if __name__ == '__main__':
