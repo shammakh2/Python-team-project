76ef59d7430322dd32daee6c17f76ffec8e12b25
b9017955 <b9017955@newcastle.ac.uk>
2019-11-06 15:55:32 +0000

    Added descriptions to webpages of all Specifications.

--- specification-3/web_app.py
+++ specification-3/web_app.py
@@ -3,18 +3,44 @@ from flask import render_template
 
 app = Flask(__name__)
 home_message= "Hello Team 48! This is the homepage to the web app. This web app's purpose is to represent our teams work" \
-              " in a web interface."
+              " in a web interface. You can navigate by clicking the hyperlinks at the top."
+
+spec_1_header="Text Frequency Analyser"
 spec_1_message= "In this challenge you are going to be exploring frequency analysis of large text files and producing" \
                 " a visual way of presenting your analysis results. Write a program that takes a large text file and " \
                 "perform a frequency analysis of the characters and words in that file. You should then present your " \
                 "findings visually using the python 2D graph plotting library, Matplotlib."
-spec_1_header="Text Frequency Analyser"
-spec_2_message= "This is the Specification 2"
-spec_2_header="Image thing"
-spec_3_message= "This is the Specification 3"
-spec_3_header="This Web Interface"
-spec_4_message= "This is the Specification 4"
+
+spec_2_header= "Image Analyser"
+spec_2_message= "In this challenge, you are going to be exploring image manipulation using the Pillow Python library. " \
+                "Write a program that modifies a selection of images of various sizes and file formats and converts " \
+                "them to JPEG thumbnails. Your program should allow the user to apply a selection of modifications to " \
+                "an image and then store the modified image. To be able to modify images in this way, you will have to " \
+                "make use of the Pillow Python library. This library is a fork of the Python Imaging Library (PIL), so" \
+                " much of the existing functionality is still there, however, the library has been expanded further." \
+                "After you completed the core features of the program, expand your program by experimenting with " \
+                "different features of the Pillow library and showing your results. For example, you could manipulate " \
+                "the specific RGB values of each pixel of an image. If applicable, make sure to discuss what features " \
+                "you have experimented with in your individual report."
+
+spec_3_header= "This Web Interface"
+spec_3_message= "In this challenge you are going to build a web application using a Python-based web framework. Flask " \
+                "is a common web framework that is easy to setup and allows extensibility. \nYou are going to build a " \
+                "simple web application that presents the code written in the previous challenge specifications. " \
+                "The purpose of the webpages is to present the code neatly with some brief documentation explaining " \
+                "the purpose of the code. What you do here is entirely experimental and open for creativity, therefore" \
+                " you can make use of any external libraries for the presentation of your code. However, be sure not to" \
+                " 'feature creep'. \nThis challenge is about your ability to investigate a Python web framework library" \
+                " and its applications rather than your HTML, CSS, and JavaScript abilities. I suggest looking through" \
+                " my guide on creating a simple web application that uses HTML templates."
+
 spec_4_header="Custom app"
+spec_4_message= "In this challenge it is up to you to choose a Python library that hasn't been mentioned before in the " \
+                "previous challenges and develop a software artefact around that library. The software artefact " \
+                "should make use of some of the skills you have learned so far in CSC1031, CSC1032, CSC1033, and " \
+                "CSC1034 so that it clearly displays your programming and theoretical abilities.\nThis challenge has " \
+                "complete creative freedom. Meaning, you can build anything you like as long as it doesn't make use " \
+                "of an aforementioned library in the previous challenges."
 
 @app.route('/')
 def home():
