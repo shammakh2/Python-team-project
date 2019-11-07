e1e79286a4081a36abded17574fdd07a345866ba
b9017955 <b9017955@newcastle.ac.uk>
2019-11-07 16:40:58 +0000

    Modified workings for displaying README completely. Now using Flask to deliver string variable which JavaScript uses to add into a <textarea> in HTML. Added workings for displaying everyones python files in the same manner as above.

--- specification-3/web_app.py
+++ specification-3/web_app.py
@@ -12,13 +12,13 @@ global spec_1_message
 spec_1_message=""
 with open("../specification-1/README.md", "r") as file:
     for line in file:
-        spec_1_message= spec_1_message.rstrip() + line.rstrip()
+        spec_1_message= spec_1_message.rstrip() + line.rstrip() + "&#10;"
 
 global spec_1_python
 spec_1_python=""
 with open("../specification-1/ata.py", "r") as file:
     for line in file:
-        spec_1_python= spec_1_python.rstrip() + line.rstrip()
+        spec_1_python= spec_1_python.rstrip() + line.rstrip()+ "&#10;"
 
 
 spec_2_header= "Image Analyser"
@@ -27,8 +27,13 @@ global spec_2_message
 spec_2_message=""
 with open("../specification-2/README.md", "r") as file:
     for line in file:
-        spec_2_message= spec_2_message.rstrip() + line.rstrip()
+        spec_2_message= spec_2_message.rstrip() + line.rstrip()+ "&#10;"
 
+global spec_2_python
+spec_2_python=""
+with open("../specification-2/img_manip.py", "r") as file:
+    for line in file:
+        spec_2_python= spec_2_python.rstrip() + line.rstrip()+ "&#10;"
 
 spec_3_header= "This Web Interface"
 
@@ -36,7 +41,13 @@ global spec_3_message
 spec_3_message=""
 with open("../specification-3/README.md", "r") as file:
     for line in file:
-        spec_3_message= spec_3_message.rstrip() + line.rstrip()
+        spec_3_message= spec_3_message.rstrip() + line.rstrip()+ "&#10;"
+
+global spec_3_python
+spec_3_python=""
+with open("../specification-3/web_app.py", "r") as file:
+    for line in file:
+        spec_3_python= spec_3_python.rstrip() + line.rstrip()+ "&#10;"
 
 spec_4_header="Custom app"
 
@@ -44,27 +55,31 @@ global spec_4_message
 spec_4_message=""
 with open("../specification-4/README.md", "r") as file:
     for line in file:
-        spec_4_message= spec_4_message.rstrip() + line.rstrip()
+        spec_4_message= spec_4_message.rstrip() + line.rstrip()+ "&#10;"
 
 @app.route('/')
 def home():
-    return render_template('index.html', title="Homepage", header="Home Page",  message= home_message, code= spec_1_python)
+    return render_template('index.html', title="Homepage", header="Home Page",  message= home_message, code= "")
 
 @app.route('/page_1')
 def page_1():
-    return render_template('index.html', title="Specification 1", header=spec_1_header, message= spec_1_message, code="")
+    return render_template('index.html', title="Specification 1",
+                           header=spec_1_header, message= spec_1_message, code=spec_1_python)
 
 @app.route('/page_2')
 def page_2():
-    return render_template('index.html', title="Specification 2", header=spec_2_header, message= spec_2_message, code="")
+    return render_template('index.html', title="Specification 2",
+                           header=spec_2_header, message= spec_2_message, code=spec_2_python)
 
 @app.route('/page_3')
 def page_3():
-    return render_template('index.html', title="Specification 3", header=spec_3_header, message= spec_3_message, code="")
+    return render_template('index.html', title="Specification 3",
+                           header=spec_3_header, message= spec_3_message, code=spec_3_python)
 
 @app.route('/page_4')
 def page_4():
-    return render_template('index.html', title="Specification 4", header=spec_4_header, message= spec_4_message, code="")
+    return render_template('index.html', title="Specification 4",
+                           header=spec_4_header, message= spec_4_message, code="")
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run()
