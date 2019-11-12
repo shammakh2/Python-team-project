39c3dfa9ce97577f721c2a7d1a59f276c3e896dd
b9035266 <RealLakeFirm11012000>
2019-11-12 15:00:45 +0000

    fixed the issue of thumbnail duplication

--- specification-2/img_manip.py
+++ specification-2/img_manip.py
@@ -3,10 +3,10 @@ from PIL import ImageFilter
 import os
 import argparse
 
-def create_thumbnails():
+def create_thumbnails(args):
     size = 300, 300
     files = os.listdir(r'C:\Users\b9035266\PycharmProjects\practical-3\specification-2\images')
-
+    counter = 0
     for f in files:
         fn, fext = os.path.splitext(f)
         im = Image.open(os.path.join(r'C:\Users\b9035266\PycharmProjects\practical-3\specification-2\images', f))
@@ -15,8 +15,9 @@ def create_thumbnails():
             im = im.convert('RGB')
 
         im.thumbnail(size)
-        file_path = os.path.join(r'C:\Users\b9035266\PycharmProjects\practical-3\specification-2\thumbnails', fn)
-        im.save(file_path + ".thumbnail", "JPEG")
+        im.save(r'C:\Users\b9035266\PycharmProjects\practical-3\specification-2\thumbnails\thumbnail{0}.thumbnail'
+                   .format(counter), "JPEG")
+        counter += 1
 
 def gaussian_blur(args):
     thumbnails = os.listdir(r'C:\Users\b9035266\PycharmProjects\practical-3\specification-2\thumbnails')
@@ -26,8 +27,10 @@ def gaussian_blur(args):
         thumb = Image.open(os.path.join(r'C:\Users\b9035266\PycharmProjects\practical-3\specification-2\thumbnails', f))
         thumb = thumb.filter(ImageFilter.GaussianBlur(radius = 10))
 
-        thumb.save(r'C:\Users\b9035266\PycharmProjects\practical-3\specification-2\thumbnails\filtered_thumbnail{0}.thumbnail'.format(counter), "JPEG")
-        counter+=1
+        thumb.save(r'C:\Users\b9035266\PycharmProjects\practical-3\specification-2\thumbnails\thumbnail{0}.thumbnail'
+                   .format(counter), "JPEG")
+        counter += 1
+
 
 def color_conversion(args):
     #take pixels of {color} and convert them to another {color}
@@ -37,7 +40,8 @@ def color_conversion(args):
         "red": (255, 0, 0),
         "green": (0, 255, 0),
         "blue": (0, 0, 255),
-        "black": (0, 0, 0)
+        "black": (0, 0, 0),
+        "white": (255, 255, 255)
     }
     counter = 0
     for f in thumbnails:
@@ -51,23 +55,24 @@ def color_conversion(args):
                 if pixels[i, j] == colors[args.colora]:
                     pixels[i, j] = colors[args.colorb]
 
-        thumb.save(r'C:\Users\b9035266\PycharmProjects\practical-3\specification-2\thumbnails\converted_thumbnail{0}.thumbnail'.format(counter), "JPEG")
+        thumb.save(r'C:\Users\b9035266\PycharmProjects\practical-3\specification-2\thumbnails\thumbnail{0}.thumbnail'
+                   .format(counter), "JPEG")
         counter += 1
 
 def run():
 
-    create_thumbnails()
-
     parser = argparse.ArgumentParser(prog="img_manip")
     subparsers = parser.add_subparsers(help="Sub Command Help")
 
-    subparsers.add_parser("blur", help='Gaussian blur filter').set_defaults(func=gaussian_blur)
-    subparsers.add_parser("conv", help='Gaussian blur filter').set_defaults(func=color_conversion)
+    subparsers.add_parser("thumbnails", help='Creates the thumbnails').set_defaults(func=create_thumbnails)
+
     conversion = subparsers.add_parser("color_conversion", help='converts pixels from Color A to Color B')
     conversion.set_defaults(func=color_conversion)
-
     conversion.add_argument('-colora', dest='colora', action='store', required=True, help='color to convert from')
     conversion.add_argument('-colorb', dest='colorb', action='store', required=True, help='color to convert to')
+
+    parser.add_argument("-loca", help="Location of images to be modified", required=False)                      #CHANGE FROM FALSE TO TRUE WHEN DONE
+    parser.add_argument("-locb", help="Location of where to store the modified images", required=False)         #CHANGE FROM FALSE TO TRUE WHEN DONE
 
     args = parser.parse_args()
     args.func(args)
