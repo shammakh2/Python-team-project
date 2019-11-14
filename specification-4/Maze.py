a7d13418a2d90ae08c854e838322edf91010b852
b9003599 <b9003599@newcastle.ac.uk>
2019-11-14 09:58:49 +0000

    Player character is complete

--- specification-4/Maze.py
+++ specification-4/Maze.py
@@ -1,11 +1,10 @@
 import pygame
-import math
 import random
 
 pygame.init()
-wide = 100
-rows = 8
-cols = 8
+wide = 50
+rows = 10
+cols = 10
 win_x = (wide*cols) + 3
 win_y = (wide*rows) + 3
 screen = pygame.display.set_mode((win_x, win_y))
@@ -20,6 +19,7 @@ class Cell:
         self.visited = False
         self.x = self.col * wide
         self.y = self.row * wide
+        self.neighbor = []
 
     def __str__(self):
         return f"I am Col {self.col} and Row {self.row}"
@@ -39,38 +39,82 @@ class Cell:
 
 
     def loc(self):
-        return (self.x, self.y, wide,wide)
+        return self.x, self.y, wide, wide
     def char_size(self):
-        return (self.x + 2 , self.y +2, wide-3,wide-3)
+        return self.x + 2 , self.y + 2, wide - 3, wide - 3
 
     def find_neighbor(self):
         self.neighbor = []
         for all in grid:
-            if all.x >= 0 and all.y >= 0 and all.x <= win_x and all.y <= win_y:
+            if 0 <= all.x <= win_x and 0 <= all.y <= win_y:
                 if (all.col == self.col + 1 and  all.row == self.row) or (all.col == self.col - 1 and all.row == self.row) or (all.col == self.col and  all.row == self.row + 1) or (all.col == self.col and  all.row == self.row - 1):
-                    if all.visited != True:
                         self.neighbor.append(all)
-        if len(self.neighbor) > 0:
-            return random.choice(self.neighbor)
+
+
+    def find_next(self):
+        self.next_possible = []
+        for ob in self.neighbor:
+            if not ob.visited:
+                self.next_possible.append(ob)
+        if len(self.next_possible) > 0:
+            return random.choice(self.next_possible)
 
 class player:
-    def __init__(self, x, y, col, row):
-        self.x = x
-        self.y = y
-        self.col = col
-        self.row = row
+    def __init__(self, current):
+        self.__on_cell = current
 
-    def char_size(self):
-        return (self.x + 3, self.y + 3, wide - 6, wide - 6)
+    def __str__(self):
+        return f"This is on {self.__on_cell.col} and {self.__on_cell.row}"
+
+    def loc_set(self, current):
+        self.__on_cell = current
+
+    def loc_get(self):
+        return self.__on_cell
+
+    def movement_up(self):
+        for x in self.__on_cell.neighbor:
+            if x.col == self.__on_cell.col and x.row == self.__on_cell.row - 1:
+                if self.__on_cell.sides[0] is False:
+                    self.__on_cell = x
+
+
+    def movement_down(self):
+        for x in self.__on_cell.neighbor:
+            if x.col == self.__on_cell.col and x.row == self.__on_cell.row + 1:
+                if self.__on_cell.sides[2] is False:
+                    self.__on_cell = x
+                    print(f"x: {self.__on_cell.x}  y: {self.__on_cell.y}")
+                    print('down')
+
+
+
+    def movement_left(self):
+        for x in self.__on_cell.neighbor:
+            if x.row == self.__on_cell.row and x.col == self.__on_cell.col - 1:
+                if self.__on_cell.sides[3] is False:
+                    self.__on_cell = x
+
+
+    def movement_right(self):
+        for x in self.__on_cell.neighbor:
+            if x.row == self.__on_cell.row and x.col == self.__on_cell.col + 1:
+                if self.__on_cell.sides[1] is False:
+                    self.__on_cell = x
+                    print('right')
+
+    
+
+
 
 
 
 def main ():
     background = pygame.Surface(screen.get_size())
     background.fill((0,0,0))
-
     game_loading = True
     game_start = False
+    press_time = 0
     stack = []
     visited = 1
     loop_de_loop = []
@@ -78,38 +122,50 @@ def main ():
         for x in range(0,cols):
             grid.append(Cell(x,y))
     for cell in grid:
+        cell.find_neighbor()
         loop_de_loop.append(cell.draw_cell)
     current = grid[0]
-
-    # def player_up():
-
-
+    player_is_born_in_this_foreign_land = player(current)
+    pygame.key.set_repeat(5)
     while True:
-        pygame.time.Clock().tick(10)
+        pygame.time.Clock().tick(15)
+        if press_time > 0:
+            press_time += 1
+        if press_time > 5:
+            press_time = 0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            if event.type == pygame.KEYDOWN:
-                if pygame.K_UP or pygame.K_w:
+            if event.type == pygame.KEYDOWN and press_time == 0:
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
+                    player_is_born_in_this_foreign_land.movement_up()
+                    current = player_is_born_in_this_foreign_land.loc_get()
 
-                if pygame.K_DOWN or pygame.K_s:
+                if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                    player_is_born_in_this_foreign_land.movement_down()
+                    current = player_is_born_in_this_foreign_land.loc_get()
 
-                if pygame.K_LEFT or pygame.K_a:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                    player_is_born_in_this_foreign_land.movement_left()
+                    current = player_is_born_in_this_foreign_land.loc_get()
 
-                if pygame.K_RIGHT or pygame.K_d:
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    player_is_born_in_this_foreign_land.movement_right()
+                    current = player_is_born_in_this_foreign_land.loc_get()
+
+
         screen.blit(background, (0,0))
-        next_cell = current.find_neighbor()
+        next_cell = current.find_next()
         for x in loop_de_loop:
             x()
-        if game_loading == True and game_start == False:
+        if game_loading is True and game_start is False:
             if current.visited:
                 pygame.draw.rect(screen, (255, 255, 150, 100), current.char_size())
-
             else:
                 pygame.draw.rect(screen, (130, 130, 255, 100), current.char_size())
             current.visited = True
             if next_cell is not None:
-                if next_cell.visited == False:
+                if not next_cell.visited:
                     if next_cell.col - current.col == 1:
                         current.sides[1] = False
                         next_cell.sides[3] = False
@@ -131,11 +187,8 @@ def main ():
             if visited == rows*cols and current == grid[0]:
                 game_loading = False
                 game_start = True
-        if game_loading == False and game_start == True:
-            player_is_born_in_this_foreign_land = player(current.x, current.y)
-            pygame.draw.rect(screen, (77, 255, 136, 100), player_is_born_in_this_foreign_land.char_size())
-
-
+        if game_loading is False and game_start is True:
+            pygame.draw.rect(screen, (77, 255, 136, 100), (current.x, current.y, wide, wide ))
 
         pygame.display.update()
 
